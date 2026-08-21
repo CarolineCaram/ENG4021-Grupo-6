fce1d7dcbcf6b7b8fbe775b9e9c92a9b231beb81
joana-arrais <joana.mello2007@gmail.com>
2026-08-21 11:29:57 +0000

    subtrai

---
+++ subtrai.py
@@ -0,0 +1,13 @@
+def subtraif(n1: float, n2: float) -> float:
+    '''
+    Função que retorna a subtração de dois números (inteiros ou reais).
+
+    :param n1: número 1 (int ou float)
+    :param n2: número 2 (int ou float)
+    :return: subtrai n2 de n1 (int ou float)
+    
+    Exemplo:
+    >>> subtraif(3, 1)
+    2
+    '''
+    return n1 - n2
